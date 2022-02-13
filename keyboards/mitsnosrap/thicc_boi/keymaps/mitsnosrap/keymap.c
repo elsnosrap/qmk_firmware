7885973b35de85dcbf4270aa49e7a05dcaad22bc
Tim Parsons <jutist@gmail.com>
2022-02-13 14:11:22 -0500

    Update Thicc Boi keymap

--- keyboards/mitsnosrap/thicc_boi/keymaps/mitsnosrap/keymap.c
+++ keyboards/mitsnosrap/thicc_boi/keymaps/mitsnosrap/keymap.c
@@ -18,7 +18,15 @@
 
 // Layer labels defined for readability
 #define _BL 0
-#define _FL 1
+#define _FN1 1
+#define _FN2 2
+#define _FN_EMPTY 99
+
+// Alternate definitions for longer codes
+#define FN1_N LT(_FN1, KC_N)
+#define FN2_V LT(_FN2, KC_V)
+#define FN2_C LT(_FN2, KC_C)
+#define CTRL_M MT(MOD_LCTL, KC_M)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
@@ -26,16 +34,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,          KC_EQL,  KC_BSPC, KC_F12,
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
         KC_ESC,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_PGUP,
-                 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LCTL, KC_LALT,          KC_LGUI,                            KC_SPC,                             MO(_FL),          KC_LEFT, KC_DOWN, KC_RGHT),
+                 KC_LSFT, KC_Z,    KC_X,    FN2_C,   FN2_V,   KC_B,    FN1_N,   CTRL_M,  KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LCTL, KC_LALT,          KC_LGUI,                            KC_SPC,                             MO(_FN1),         KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [_FL] = LAYOUT(
+    [_FN1] = LAYOUT(
         _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
         _______,          M_GSTAT, M_GBRAN, M_GLOG,  M_GPULL, M_GPRUN, M_GCDEV, _______, _______, M_GDIFF, KC_MPLY, KC_MRWD, KC_MFFD, KC_MUTE, _______,
         _______,          M_GSLIS, M_GSSHO, M_GSAPL, M_GSDRO, M_GSPUS, M_GSPOP, _______, _______, _______, _______, M_CLLOG, _______,          _______,
                  _______, M_ACFER, M_ACPRE, M_ACPAR, M_ALDL,  M_ALDP,  _______, _______, KC_F14,  KC_F15,  _______,          _______, KC_VOLU, _______,
         _______, _______,          _______,                            _______,                            _______,          KC_HOME, KC_VOLD, KC_END),
+
+    [_FN2] = LAYOUT(
+        _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_F12,  _______, _______,
+        _______,          _______, _______, M_EMOJ,  _______, _______, _______, M_ALFR,  _______, M_GDIFF, _______, _______, _______, _______, _______,
+        _______,          KC_LALT, KC_LSFT, KC_LGUI, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          _______,
+                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______,          _______,                            _______,                            _______,          _______, _______, _______),
+
+    [_FN_EMPTY] = LAYOUT(
+        _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______,          _______,                            _______,                            _______,          _______, _______, _______),
+
 };
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
