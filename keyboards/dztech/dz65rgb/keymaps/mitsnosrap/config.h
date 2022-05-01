217e4bbb5e3ec4ab7934da3795861de0d464a130
Tim Parsons <jutist@gmail.com>
2022-05-01 16:13:36 -0400

    Keymap for DZ65rgb

---
+++ keyboards/dztech/dz65rgb/keymaps/mitsnosrap/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Prefer a slightly longer tap term setting https://docs.qmk.fm/#/tap_hold
+#define TAPPING_TERM 250
+
+// Define the following so mod-tap works better for my typing style
+#define IGNORE_MOD_TAP_INTERRUPT
+
