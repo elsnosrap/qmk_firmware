cf4c142198911657d17808225022fa49d1d0ac23
Ellie Parsons <5825829+mitsnosrap@users.noreply.github.com>
2022-07-12 08:24:05 -0400

    KEYMAP: Update dz60rgb keymap

--- keyboards/dztech/dz60rgb_ansi/keymaps/mitsnosrap/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/mitsnosrap/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    FN2_C,   KC_V,    KC_B,    FN1_N,   CTRL_M,  KC_COMM, KC_DOT,   KC_SLSH,          KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             MO(_FN3), _______,   KC_HOME, KC_END),
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT),
 
     [_FN1] = LAYOUT_60_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_FN2] = LAYOUT_60_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,
-        _______, _______, _______, M_EMOJ,  _______, _______, _______, M_ALFR,  _______, M_GDIFF, _______,  _______, _______, _______,
+        _______, _______, _______, M_EMOJ,  _______, _______, _______, M_ALFR,  _______, M_GDIFF, KC_MPLY,  KC_MRWD, KC_MFFD, _______,
         _______, KC_LALT, KC_LSFT, KC_LGUI, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,  KC_VOLU,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, KC_F14,  KC_F15,   KC_VOLD,          _______,
-        _______, _______, _______,                            _______,                            _______,  _______, _______, _______),
+        _______,          _______, MO(_FN3),_______, _______, _______, _______, _______, KC_F14,  KC_F15,   KC_VOLD,          _______,
+        _______, _______, _______,                            _______,                            KC_HOME,  KC_PGDN, KC_PGUP, KC_END),
 
     [_FN3] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
