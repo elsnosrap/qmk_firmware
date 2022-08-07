6302fe8ba0d00a73f73a74be46bb1a61d7724e27
Ellie Parsons <5825829+mitsnosrap@users.noreply.github.com>
2022-08-07 15:40:15 -0400

    Update Thicc Boi
    Update the configuration a bit, tweak the keymap and make some minor
    changes to the rules so they compile

--- keyboards/mitsnosrap/thicc_boi/config.h
+++ keyboards/mitsnosrap/thicc_boi/config.h
@@ -20,12 +20,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x6061
-#define DEVICE_VER 0x0001
-#define MANUFACTURER mitsnosrap
-#define PRODUCT An ultra-thicc 80% Keyboard
-#define DESCRIPTION Thicc Boi
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6061
+#define DEVICE_VER      0x0001
+
+#define MANUFACTURER    "mitsnosrap"
+#define PRODUCT         "Thicc Boi"
+#define SERIAL_NUM      "Unavailable"
 
 /* key matrix size */
 #define MATRIX_ROWS 6
--- keyboards/mitsnosrap/thicc_boi/rules.mk
+++ keyboards/mitsnosrap/thicc_boi/rules.mk
@@ -1,6 +1,10 @@
 # MCU name
 # MCU = atmega32u4
 MCU = STM32F303
+BOARD = QMK_PROTON_C
+
+# Bootloader selection
+BOOTLOADER = stm32-dfu
 
 # Build Options
 #   change yes to no to disable
